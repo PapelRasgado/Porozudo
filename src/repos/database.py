e2fc90587fbc1201e4530a4519b5f90578ceb7eb
PapelRasgado <joaodantas31.jpd@gmail.com>
2025-09-26 19:34:29 -0300

    Adding migration back

--- src/repos/database.py
+++ src/repos/database.py
@@ -1,6 +1,7 @@
 from sqlmodel import Session, SQLModel, create_engine
 
-DATABASE_URL = "sqlite:///./resources/database.db"
+from src.config import DATABASE_URL
+
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
 
