05ec5574b167c216c0218ff5b9521f9ee90609fe
PapelRasgado <joaodantas31.jpd@gmail.com>
2025-09-26 20:20:07 -0300

    Removing sqlite args

--- src/repos/database.py
+++ src/repos/database.py
@@ -2,7 +2,7 @@ from sqlmodel import Session, SQLModel, create_engine
 
 from src.config import DATABASE_URL
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 
 
 def create_db_and_tables():
